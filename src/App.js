d3143f2a9dc8942a914425e7a1f189caf6fa955b
Aigelov <aigelov@gmail.com>
2020-07-01 18:53:06 +0600

    Sidebar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Sidebar from "./Sidebar/Sidebar";
 
 function App() {
   return (
     <div className="App">
-
+      <Sidebar />
     </div>
   );
 }
